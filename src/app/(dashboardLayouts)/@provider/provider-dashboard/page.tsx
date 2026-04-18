4c31d3f7e2ba8772b4dcac419297a1177910600b
tabib-e-alahi <etabib.alahi@gmail.com>
2026-04-19 00:06:58 +0600

    Remove unused import and client directive

--- src/app/(dashboardLayouts)/@provider/provider-dashboard/page.tsx
+++ src/app/(dashboardLayouts)/@provider/provider-dashboard/page.tsx
@@ -1,10 +1,5 @@
-"use client"
-
-import { useAuth } from "@/providers/AuthProvider";
 
 export default function ProviderDashboardPage() {
-  const {user} = useAuth()
-  console.log(user);
   return (
     <div>
       <h1>provider page</h1>
